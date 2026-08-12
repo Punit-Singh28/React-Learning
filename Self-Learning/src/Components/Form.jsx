5da986b00df5971700b4fcfddc407283104154cd
Punit_singh <mr.punitsingh2628@gmail.com>
2026-08-12 22:17:20 +0530

    worked on Self-Learning

--- Self-Learning/src/Components/Form.jsx
+++ Self-Learning/src/Components/Form.jsx
@@ -3,62 +3,78 @@ import React from "react";
 export const Form = ({ props, button }) => {
   const [formData, setFormData] = React.useState({});
 
-
-  const handleChange = (e) =>{
-    const { name, type, value } = e.target;
-
-
-  setFormData((prev) => ({
-    ...prev,
-    [name]: type === "radio" ? value : value,
-  }));
-
-  }
-  
-  const handlesubmit = (e) => {
-    e.preventDefault();
-    console.log("Form Data: ", formData)
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
   };
-  return (
-    <form onSubmit={handlesubmit}>
-      {props?.map((el) => {
-        return (
-          <div
-            key={el.id}
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              gap: "10px",
-              width: "30%",
-              marginBottom: "10px",
-            }}
-          >
-            {el.name === "gender" ? (
-              <>
-                <label>{el.name}</label>
-                <div style={{ display: "flex", gap: "5px" }}>
-                  {" "}
-                  <div>
-                    <label htmlFor="male">Male</label>
-                    <input type={el.type} value="male" onChange={handleChange}/>
-                  </div>
-                  <div>
-                    <label htmlFor="female">Female</label>
-                    <input type={el.type} value="female" onChange={handleChange}/>
-                  </div>
-                </div>
-              </>
-            ) : (
-              <>
-                <label htmlFor={el.name}>{el.name}</label>
-                <input id={el.name} type={el.name} name={el.name} onChange={handleChange}/>
-              </>
-            )}
-          </div>
-        );
-      })}
 
-      <button>{button}</button>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form Data: ", formData);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} style={{
+            display: "flex",
+            marginTop: "15px",
+             gap: "5px",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",}}>
+      {props?.map((el) => (
+        <div
+          key={el.id}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            gap: "10px",
+            width: "30%",
+            marginBottom: "10px",
+          }}
+        >
+          {el.name === "gender" ? (
+            <>
+              <label>{el.name}</label>
+              <div style={{ display: "flex", gap: "5px" }}>
+                <div>
+                  <label htmlFor="male">Male</label>
+                  <input
+                    type="radio"
+                    name="gender"
+                    value="male"
+                    onChange={handleChange}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="female">Female</label>
+                  <input
+                    type="radio"
+                    name="gender"
+                    value="female"
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+            </>
+          ) : (
+            <>
+              <label htmlFor={el.name}>{el.name}</label>
+              <input
+                id={el.name}
+                type={el.type}
+                name={el.name}
+                onChange={handleChange}
+              />
+            </>
+          )}
+        </div>
+      ))}
+
       <button type="submit">{button}</button>
     </form>
   );
